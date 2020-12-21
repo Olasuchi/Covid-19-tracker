9f7906788929b7cac382fde6712aaed1b206fbc8
olasuchi <ilesanmiolaide001@gmail.com>
2020-12-21 13:06:57 +0100

    creating lingraph

--- covid-19-tracker/src/App.js
+++ covid-19-tracker/src/App.js
@@ -113,6 +113,7 @@ function App() {
           <h3>Live Cases by Country</h3>
             <Table countries={tableData}></Table>
           <h3>Worldwide new cases</h3>
+                          <LineGraph />
               {/*Graph */}
         </CardContent>
       </Card>
