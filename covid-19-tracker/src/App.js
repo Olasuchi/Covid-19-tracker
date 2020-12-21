926d738f102dc9755201e702d2a45b74a8311376
olasuchi <ilesanmiolaide001@gmail.com>
2020-12-21 05:27:36 +0100

    app dropdown

--- covid-19-tracker/src/App.js
+++ covid-19-tracker/src/App.js
@@ -1,10 +1,26 @@
 import React from "react";
+import {
+  MenuItem,
+  FormControl,
+  Select,
+} from "@material-ui/core";
+
 import './App.css';
 
 function App() {
   return (
     <div className="app">
       <h1> COVID-19 TRACKER</h1>
+        <FormControl className="app_dropdown">
+          <Select variant="outlined" value="abc">
+            <MenuItem value="worldwide">worldwide</MenuItem>
+            <MenuItem value="worldwide">option1</MenuItem>
+            <MenuItem value="worldwide">option2</MenuItem>
+            <MenuItem value="worldwide">option3</MenuItem>
+
+          </Select>
+
+        </FormControl>
 
       {/*Header */}
       {/*title + select input dropdown field */}
