b6b9b06e21d73ccc86abb8d34987dba265b780b3
olasuchi <ilesanmiolaide001@gmail.com>
2020-12-21 04:34:53 +0100

    building structure

--- covid-19-tracker/src/App.js
+++ covid-19-tracker/src/App.js
@@ -5,6 +5,18 @@ function App() {
   return (
     <div className="app">
       <h1> COVID-19 TRACKER</h1>
+
+      {/*Header */}
+      {/*title + select input dropdown field */}
+
+      {/*Info box1 */}
+      {/*Info box2 */}
+      {/*Info box3 */}
+
+      {/*table */}
+      {/*Graph */}
+
+      {/*Map */}
       
     </div>
   );
